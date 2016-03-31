361eea09860dc17ce8832ab679ec847b39b7b902
Cordavi <michaelamundsen@gmail.com>
2016-03-30 22:37:51 -0400

    Created logic to find mutliples of 3 & 5 and then sum totals below 1000

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,33 @@
 # Enter your procedural solution here!
+def collect_multiples(maximum)
+    numbers = Array.new
+    for i in  0..(maximum - 1)
+       if i > 0 && ((i % 3 == 0) || (i % 5 == 0))
+        numbers << i
+       end 
+    end
+    return numbers
+  end
+
+  def sum_multiples(maximum)
+    multiples = collect_multiples(maximum)
+    multiples.inject(0, :+)
+  end
+
+class Multiples
+
+  def initialize(limit)
+    @limit = limit
+  end 
+
+  def collect_multiples
+    Object.send(:collect_multiples,@limit)
+  end
+
+  def sum_multiples 
+    Object.send(:sum_multiples, @limit)
+  end  
+end
+
+
+
